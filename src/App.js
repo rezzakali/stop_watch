a6bedf51db966c61197b759870e99090951d72f9
Rezzak Ali <myrezzakalirk@gmail.com>
2022-04-23 18:26:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import RotateLeftIcon from "@mui/icons-material/RotateLeft";
+import StopIcon from "@mui/icons-material/Stop";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [millisecond, setMillisecond] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [stop, setStop] = useState(false);
+
+  const startMethod = () => {
+    setStop(true);
+  };
+  const stopMethod = () => {
+    setStop(false);
+  };
+  const resetMethod = () => {
+    setMillisecond(0);
+    setSecond(0);
+    setMinute(0);
+    setHour(0);
+  };
+  useEffect(() => {
+    let interval = null;
+    if (stop) {
+      interval = setInterval(() => {
+        if (minute > 59) {
+          setHour(hour + 1);
+          setMinute(0);
+          clearInterval(interval);
+        }
+        if (second > 59) {
+          setMinute(minute + 1);
+          setSecond(0);
+          clearInterval(interval);
+        }
+        if (millisecond > 99) {
+          setSecond(second + 1);
+          setMillisecond(0);
+          clearInterval(interval);
+        }
+        if (millisecond <= 99) {
+          setMillisecond(millisecond + 1);
+        }
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container mt-5">
+      <h1>StopWatch</h1>
+      <div className="timer">
+        <span>{hour}:</span>
+        <span>{minute}:</span>
+        <span>{second}:</span>
+        <span>{millisecond}</span>
+      </div>
+      <div className="buttons">
+        <a onClick={resetMethod}>
+          <RotateLeftIcon id="resetIcon" />
         </a>
-      </header>
+        <a onClick={startMethod}>
+          <PlayArrowIcon id="startIcon" />
+        </a>
+        <a onClick={stopMethod}>
+          <StopIcon id="stopIcon" />
+        </a>
+      </div>
     </div>
   );
 }
